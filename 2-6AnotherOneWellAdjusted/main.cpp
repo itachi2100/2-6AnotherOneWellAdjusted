acf0d5f08ad87065b084360b431de3b830f95446
Itachi <samcravatta@gmail.com>
2019-12-25 16:26:10 -0600

    Initial Commit

---
+++ 2-6AnotherOneWellAdjusted/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2-6AnotherOneWellAdjusted
+//
+//  Created by Sam Cravatta on 12/25/19.
+//  Copyright © 2019 Leaf_Programming. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
